04fff2d53ad95c94478e95ef9e801a58a6eb8419
thakkar4 <thakkar4@yahoo.com>
2020-07-03 16:08:55 -0500

    More programs

---
+++ src/ForSumOfNaturalNumbers.java
@@ -0,0 +1,18 @@
+public class ForSumOfNaturalNumbers {
+
+    public static void main(String[] args) {
+     /*  int num =10, count, total =0;
+           for(count = 1; count<=num; count++) {
+               total = total +count;
+        }
+        System.out.println("sum is: "+total); */
+
+        for(int i=100; i>=2; i-=4) {
+            System.out.println(i);
+        }
+       /* for(int i=2; i<=100; i+=4){
+            System.out.println(i);
+        }*/
+    }
+
+    }
--- src/SwitchCalculator.java
+++ src/SwitchCalculator.java
@@ -4,7 +4,7 @@ public class SwitchCalculator {
 
     public static void main(String[] args) {
 
-        double num1, num2, operator;
+        double num1, num2;
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter first number");
@@ -12,10 +12,31 @@ public class SwitchCalculator {
         System.out.println("Enter Second number");
         num2 = scan.nextDouble();
         System.out.println("Enter an operator (+, -, *, /): ");
-        operator = scan.next().charAt(0);
+        char operator = scan.next().charAt(0);
         scan.close();
-        
 
+        double output = 0;
+        switch (operator) {
+            
+            case '+':
+                output = num1 + num2;
+                break;
+                
+            case '-':
+                output = num1 - num2;
+                break;
 
+            case '*':
+                output = num1 * num2;
+                break;
+
+            case '/':
+                output = num1/num2;
+                
+            default:
+                System.out.println( "you have entered wrong operator");
+                
+        }
+        System.out.println(num1+" "+operator+ " "+num2+": " +output);
     }
 }
